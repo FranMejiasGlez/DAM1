136a9c2bd60173278d5e2b968121de1028221fe0
FranDAM1 <franciscomejias.24@campuscamara.es>
2025-02-18 12:48:33 +0100

    Clases ejercicios

---
+++ PROG/Proyectos NetBeans/nb/CLASES/src/Numero.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mejias Gonzalez Francisco
+ */
+public class Numero {
+   //Atributos:
+        private int valor=2;
+   //Constructores:
+        public Numero(){
+        }//Fin Constructor
+        public Numero(int valor){
+            this.valor=valor;
+        }//Fin Constructor
+   //Metodos:
+        public void aniade(int valor){
+            this.valor=this.valor+valor;
+        }//Fin Metodo
+        public void resta(int valor){
+            this.valor=this.valor-valor;
+        }//Fin Metodo
+   //Getter/Setters:
+        public int getValor(){
+            return this.valor;
+        }//Fin Getter
+        public int getDoble(){
+            return this.valor*2;
+        }//Fin Getter
+        public int getTriple(){
+            return this.valor*3;
+        }//Fin Getter
+        public int getCuadruple(){
+            return this.valor*4;
+        }//Fin Getter
+        public void setValor(int valor){
+           this.valor=valor;
+        }//Fin Setter
+}//Fin Clase
