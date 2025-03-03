6d5f631e86142ad2dfd794ab82cb39171b13f6c4
fran gonzalez <franciscomejias.24@campuscamara.es>
2025-03-03 18:58:47 +0100

    Ud9Act Strings

---
+++ PROG/Proyectos NetBeans/nb/Ud9Act/src/Ud9Act1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mejias Gonzalez Francisco
+ */
+public class Ud9Act1 {
+    
+    public static void main(String[] args) {
+        //Entorno
+            String cadena,palabra;
+        //Algoritmo
+            cadena = "   EL  PERRO ES   BLANCO ";
+            System.out.println(cadena.trim().indexOf(32));
+            System.out.println(cadena.trim().substring(0,2));
+            palabra=cadena.trim().substring(0,2); //EL
+}
+}
