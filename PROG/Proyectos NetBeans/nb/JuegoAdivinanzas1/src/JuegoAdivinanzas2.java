6b9d7bd49e8c730bc42bd30cedac2a3934d9a7cb
fran36dual <franciscomejias.24@campuscamara.es>
2025-03-06 10:10:50 +0100

    Juegos Adivinanzas2

--- PROG/Proyectos NetBeans/nb/JuegoAdivinanzas1/src/JuegoAdivinanzas2.java
+++ PROG/Proyectos NetBeans/nb/JuegoAdivinanzas1/src/JuegoAdivinanzas2.java
@@ -1,49 +1,5 @@
-
-   /* 
-   Programa: JuegoAdivinanzas2
-   Autor: Mejias Gonzalez Francisco
-   Entorno:
-            
-            cifras, numeroSecreto,numeroAux, numeroEstimado, intentos
-            es numérico entero
-   Algoritmo:
-        Repetir
-            Escribir "J1 Introduce un número de máximo 5 cifras: "
-            Leer numeroSecreto
-            cifras <-- 1
-            numeroAux <-- numeroSecreto
-            Mientras numeroaux >= 10 hacer
-                numeroAux <-- numeroAux \ 10;
-            cifras <-- cifras + 1 
-        Hasta cifras <= 5 y cifras >= 1
-        intentos <-- 10
-        Repetir
-            Repetir
-                Escribir "J2 Introduce numero de máximo 5 cifras: "
-                Leer numeroEstimado
-                cifras <-- 1
-                numeroAux <-- numeroEstimado
-                Mientras numeroaux >= 10 hacer
-                    numeroAux<-- numeroAux \ 10;
-                cifras <-- cifras + 1 
-            Hasta  cifras <= 5 y cifras >= 1
-                Escribir "Numero de digitos correctos: "
-                    ,comparaNumeros(numeroEstimado, numeroSecreto));
-            intentos = intentos - 1
-            Si numeroSecreto != numeroEstimado entonces
-                Escribir "Has fallado, te quedan: ",intentos," intentos"
-            Fin Si
-        Hasta intentos = 0 o numeroSecreto = numeroEstimado
-        Si numeroSecreto = numeroEstimado entonces
-            Escribir "Has acertado! Has necesitado: ",(10-intentos)," intentos"
-        Sino
-             Escribir "Has fallado! no te quedan intentos, el numero era: ",
-                    ,numeroSecreto
-        Fin Si
-  Fin Programa
-    * /
 /**
- * @author Mejias Gonzalez Francisco
+ * @author Equipo6
  */
 public class JuegoAdivinanzas2 {
 
@@ -59,7 +15,7 @@ public class JuegoAdivinanzas2 {
         return cifras;
     }//Fin Funcion
 
-    public static byte comparaNumeros(short numeroEstimado, short numeroSecreto){
+    public static byte comparaNumeros(int numeroEstimado, int numeroSecreto){
         //Entorno:
         byte numeroMenosDigitos, digitosAcertados, digitoSecreto,
                 digitoEstimado, j;
@@ -82,27 +38,27 @@ public class JuegoAdivinanzas2 {
 
     public static void main(String[] args) {
         //Entorno:
-        short numeroSecreto, numeroEstimado;
+        int numeroSecreto, numeroEstimado;
         byte intentos;
         //Algoritmo:
         do {
             System.out.println("J1 Introduce un número de máximo 5 cifras: ");
-            numeroSecreto = Leer.datoShort();
-        } while (JuegoAdivinanzas2.cifras(numeroSecreto) > 5
-                && JuegoAdivinanzas2.cifras(numeroSecreto) < 1);
+            numeroSecreto = Leer.datoInt();
+        } while (cifras(numeroSecreto) > 5 || cifras(numeroSecreto) < 1
+                || numeroSecreto < 0);
         intentos = 10;
         do {
             do {
                 System.out.println("J2 Introduce numero de máximo 5 cifras: ");
-                numeroEstimado = Leer.datoShort();
-            } while (JuegoAdivinanzas2.cifras(numeroSecreto) > 5
-                    && JuegoAdivinanzas2.cifras(numeroSecreto) < 1);
-            System.out.println("Numero de digitos correctos: "
-                    + comparaNumeros(numeroEstimado, numeroSecreto));
+                numeroEstimado = Leer.datoInt();
+            } while (cifras(numeroSecreto) > 5 || cifras(numeroSecreto) < 1 
+                    || numeroEstimado < 0 );
+            System.out.println(comparaNumeros(numeroEstimado, numeroSecreto)
+                    +" Digitos bien colocados");
             intentos--;
             if(numeroSecreto != numeroEstimado){
             System.out.println("Has fallado, te quedan: "+intentos+" intentos");
-            }
+            }//Fin Si
         }while (intentos !=0 && numeroSecreto != numeroEstimado);
         if (numeroSecreto==numeroEstimado){
             System.out.println("Has acertado! Has necesitado: "+(10-intentos)
