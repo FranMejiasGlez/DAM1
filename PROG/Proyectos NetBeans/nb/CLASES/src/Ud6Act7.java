eb6120c269a505bd7f86a97cda4b76592de54181
fran gonzalez <franciscomejias.24@campuscamara.es>
2025-03-12 18:11:30 +0100

    Clase Empleado y BDD Practicando

---
+++ PROG/Proyectos NetBeans/nb/CLASES/src/Ud6Act7.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Mejias Gonzalez Francisco
+ */
+public class Ud6Act7 {
+    public static void main(String[] args) {
+        //entorno
+            float mayorSueldo;
+            float menorSueldo;
+             
+        //algoritmo
+            
+          
+            
+        }//Fin Programa
+    }
+
