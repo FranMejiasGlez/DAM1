d7ab4fc5306b64c2bdb570c8129205afbe92fc06
FranDAM1 <franciscomejias.24@campuscamara.es>
2025-03-14 12:50:35 +0100

    clase empleado

--- PROG/Proyectos NetBeans/nb/CLASES/src/Empleado.java
+++ PROG/Proyectos NetBeans/nb/CLASES/src/Empleado.java
@@ -12,10 +12,8 @@ public class Empleado {
     private byte seccion;
     private float sueldo;
     //Constructores
-
-    public Empleado() {
-    }//Fin Construsctor
-
+    public Empleado(){
+    }//Fin Constructor
     public Empleado(String nombre, String apellidos, short departamento,
             byte seccion, float sueldo) {
         this.nombre = nombre;
@@ -25,12 +23,15 @@ public class Empleado {
         this.sueldo = sueldo;
     }//Fin Constructor
     //Metodos
-
-    public String mostrarDatos() {
-        return "Nombre: " + this.nombre + "\nApellidos: " + this.apellidos
-                + "\nDepartamento: " + this.departamento
-                + "\nSeccion: " + this.seccion + "\nSueldo: " + this.sueldo;
-    }//Fin Metodo
+    
+        public void copia(Empleado original){
+            this.nombre=original.nombre;
+            this.apellidos=original.apellidos;
+            this.sueldo=original.sueldo;
+            this.departamento=original.departamento;
+            this.seccion=original.seccion;
+        }//Fin Metodo
+        
     //Getters&Setters
 
     public String getNombre() {
@@ -41,43 +42,7 @@ public class Empleado {
         return this.apellidos;
     }//Fin Metodo
 
-    public short getDepartamento() {
-        return this.departamento;
-    }//Fin Metodo
-
-    public byte getSeccion() {
-        return this.seccion;
-    }//Fin Metodo
-
     public float getSueldo() {
         return this.sueldo;
-    }//Fin Metodo
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }//Fin Metodo
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }//Fin Metodo
-
-    public void setDepartamento(short departamento) {
-        if (departamento <= 9999) {
-            this.departamento = departamento;
-        } else {
-            System.out.println("Departamento entre 1 y 9999");
-        }
-    }//Fin Metodo
-
-    public void setSeccion(byte seccion) {
-        if (seccion <= 99) {
-            this.seccion = seccion;
-        } else {
-            System.out.println("Seccion entre 1 y 99");
-        }//Fin Si
-    }//Fin Metodo
-
-    public void setSueldo(float sueldo) {
-        this.sueldo = sueldo;
     }//Fin Metodo
 }//Fin Clase
--- PROG/Proyectos NetBeans/nb/CLASES/src/Ud6Act7.java
+++ PROG/Proyectos NetBeans/nb/CLASES/src/Ud6Act7.java
@@ -1,17 +1,55 @@
+
 /**
  *
  * @author Mejias Gonzalez Francisco
  */
 public class Ud6Act7 {
+
     public static void main(String[] args) {
         //entorno
-            float mayorSueldo;
-            float menorSueldo;
-             
+        Empleado mayorSueldo,menorSueldo,empleado;
+        //Empleado[] empleados;
+        String nombre, apellidos;
+        float sueldo;
+        byte seccion;
+        short departamento;
         //algoritmo
-            
-          
-            
-        }//Fin Programa
-    }
-
+        mayorSueldo= new Empleado();
+        menorSueldo= new Empleado();
+        System.out.println("Introduce nombre empleado: ");
+        nombre = Leer.dato();
+        System.out.println("Introduce apellidos empleado: ");
+        apellidos = Leer.dato();
+        System.out.println("Introduce sueldo empleado: ");
+        sueldo = Leer.datoFloat();
+        System.out.println("Introduce departamento: ");
+        departamento = Leer.datoShort();
+        System.out.println("Introduce seccion: ");
+        seccion = (byte) Leer.datoShort();
+        empleado = new Empleado(nombre,apellidos,departamento,seccion,sueldo);
+        mayorSueldo.copia(empleado);
+        menorSueldo.copia(empleado);
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Introduce nombre empleado: ");
+            nombre = Leer.dato();
+            System.out.println("Introduce apellidos empleado: ");
+            apellidos = Leer.dato();
+            System.out.println("Introduce sueldo empleado: ");
+            sueldo = Leer.datoFloat();
+            System.out.println("Introduce departamento: ");
+            departamento = Leer.datoShort();
+            System.out.println("Introduce seccion: ");
+            seccion = (byte) Leer.datoShort();
+            empleado=new Empleado(nombre,apellidos,departamento,seccion,sueldo);
+            if (empleado.getSueldo()>mayorSueldo.getSueldo()) {
+                mayorSueldo=empleado;
+            }else{
+                menorSueldo=empleado;
+            }
+        }//Fin Para
+        System.out.println("Mayor Sueldo: "+mayorSueldo.getNombre()
+                +" "+mayorSueldo.getApellidos()+" "+mayorSueldo.getSueldo());
+        System.out.println("Menor Sueldo: "+menorSueldo.getNombre()
+                +" "+menorSueldo.getApellidos()+" "+menorSueldo.getSueldo());
+    }//Fin Programa
+}
