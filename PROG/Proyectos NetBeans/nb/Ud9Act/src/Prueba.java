e53e4b597e2303780fa395edcb72949b9a86760f
fran gonzalez <franciscomejias.24@campuscamara.es>
2025-03-21 14:23:23 +0100

    reorg

--- PROG/Proyectos NetBeans/nb/Ud9Act/src/Prueba.java
+++ PROG/Proyectos NetBeans/nb/Ud9Act/src/Prueba.java
@@ -9,4 +9,13 @@
  */
 public class Prueba {
 
+    public static void main(String[] args) {
+        //Entorno
+        String nCadena, cadena = "hola";
+        int i;
+        //Algoritmo
+        for (i = 1; i <cadena.length() +1; i++) {
+            System.out.println(cadena.substring(i) + cadena.substring(0, i));
+        }
+    }
 }
