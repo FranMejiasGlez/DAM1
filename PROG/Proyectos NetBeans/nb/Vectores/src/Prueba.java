e52973b36313d822a63a40d74173787065194340
FranDAM1 <franciscomejias.24@campuscamara.es>
2025-03-24 14:58:13 +0100

    tabla prog

---
+++ PROG/Proyectos NetBeans/nb/Vectores/src/Prueba.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Mejias Gonzalez Francisco
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        //entorno
+            float [][] tabla={{0,1,2,3}},{{4,2,3,5}};
+        //algoritmo
+            tabla = new float[3][4];
+    }
+}
