6218753db3936c24600311d1cca5cde0e00e60ee
franDUAL13 <franciscomejias.24@campuscamara.es>
2025-03-25 14:40:09 +0100

    Adivinanzas 3

--- PROG/Proyectos NetBeans/nb/JuegoAdivinanzas1/src/Adivinanza.java
+++ PROG/Proyectos NetBeans/nb/JuegoAdivinanzas1/src/Adivinanza.java
@@ -17,7 +17,6 @@ public class Adivinanza {
 
     //Metodos
     public void juega(int num) {
-        System.out.println("Jugador, adivina el numero: ");
         this.numeroPrueba = num;
     }//Fin Metodo
 
@@ -32,25 +31,38 @@ public class Adivinanza {
     public String getPista() {
         //Entorno
         String pista;
+        Integer numeroPrueba1, incognita1;
+        byte j;
         //Algoritmo
-        pista=Adivinanza.meteAsteriscos(cifras(this.incognita));
-       return pista ;
+        j=1;
+        numeroPrueba1 = this.numeroPrueba;
+        incognita1 = this.incognita;
+        pista = Adivinanza.meteAsteriscos(cifras(this.incognita));
+        for (int i = 0 - 1; i <numeroPrueba1.toString().length(); i++) {
+            if (numeroPrueba1.toString().charAt(i) == incognita1.toString()
+                    .charAt(i)) {
+                pista =meteAsteriscos((byte) (cifras(this.incognita) - j))
+                        +numeroPrueba1.toString().charAt(i);
+                j++;
+            }//Fin Si
+        }//Fin Para
+        return pista;
     }//Fin Metodo
 
     public static String meteAsteriscos(byte n) {
         //Entorno
         String pistaAsteriscos;
         //Algoritmo
-        pistaAsteriscos="";
+        pistaAsteriscos = "";
         for (int i = 1; i <= n; i++) {
-            pistaAsteriscos=pistaAsteriscos+"* ";
+            pistaAsteriscos = pistaAsteriscos + "*";
         }//Fin Para
         return pistaAsteriscos;
     }//Fin Metodo
 
     public static byte cifras(int n) {
         //Entorno
-        return (byte)Integer.toString(n).length();
+        return (byte) Integer.toString(n).length();
     }//Fin Metodo
     //Getters&Setters
 
@@ -58,13 +70,14 @@ public class Adivinanza {
         //entorno
         Integer incognita;
         incognita = this.incognita;
-        return (byte)incognita.toString().length();
+        return (byte) incognita.toString().length();
     }//Fin Metodo
 
     public byte getIntentos() {
         return this.intentos;
     }//Fin Metodo
-    public int getIncognigta(){
-    return this.incognita;
+
+    public int getIncognigta() {
+        return this.incognita;
     }
 }//Fin Clase
--- PROG/Proyectos NetBeans/nb/JuegoAdivinanzas1/src/Prueba.java
+++ PROG/Proyectos NetBeans/nb/JuegoAdivinanzas1/src/Prueba.java
@@ -1,25 +1,34 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Mejias Gonzalez Francisco
  */
 public class Prueba {
 
+    public static byte cifras(int num) {
+        //Entorno
+        return (byte) Integer.toString(num).length();
+    }//Fin Metodo
+
     public static void main(String[] args) {
         //Entorno
         Adivinanza uno;
+        Integer num;
         //Algoritmo
-        System.out.println("!!JUEGO DE ADIVINANZAS¡¡"
+        System.out.println("!!JUEGO DE ADIVINANZAS¡¡\n"
                 + "\nEl Sistema Genera un numero entre 1 y 1.000.000\n"
-                + "\nCada digito bien colocado se desvelara, tienes 10 intentos:"
+                + "\nCada digito bien colocado se desvelara,"
+                + " tienes 10 intentos:"
                 + "\n ");
         uno = new Adivinanza();
         System.out.println(uno.getPista());
-        System.out.println("\nJugador te toca adivinar: \n");
-
-    }
+        System.out.println(uno.getIncognigta());
+        do {
+            System.out.print("\nJugador te toca adivinar\n"
+                    + "\nIntroduce un numero entre 1 y 1.000.000: \n");
+            num = Leer.datoInt();
+        } while (Prueba.cifras(num) > Prueba.cifras(uno.getIncognigta()));
+        uno.juega(num);
+        
+        System.out.println(uno.getPista());
+    }//Fin Programa
 }
