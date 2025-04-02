9eeb4ada74d8a9aec6ae94af4c6c0e20286456ad
FranDAM1 <franciscomejias.24@campuscamara.es>
2025-04-02 14:55:08 +0200

    vectores

---
+++ PROG/Proyectos NetBeans/nb/Vectores/src/Alumno.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mejias Gonzalez Francisco
+ */
+public class Alumno {
+    //Atributos
+
+    private byte cod, nota;
+    //Constructores
+
+    public Alumno(byte codigo, byte nota) {
+        this.cod = codigo;
+        this.nota = nota;
+    }//Fin Constructor
+    //Metodos
+    //GetterSetter
+}//Fin Clase
---
+++ PROG/Proyectos NetBeans/nb/Vectores/src/TablaPOO.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrador
+ */
+public class TablaPOO {
+
+    public static void main(String[] args) {
+        //Entorno
+        Alumno alum;
+        Alumno[] alumnos;
+        byte codigo,nota;
+        //Algoritmo
+        alumnos = new Alumno[8];
+        for (int i = 0; i < alumnos.length - 1; i++) {
+               System.out.println("Introduce Cod: ");
+               codigo=(byte)Leer.datoShort();
+               System.out.println("Introduce Nota: ");
+               nota=(byte)Leer.datoShort();
+               alum=new Alumno(codigo,nota);
+               alumnos[i]=alum;
+        }//Fin Para
+        for (int i = 0; i < alumnos.length; i++) {
+            System.out.println(alumnos[i]);
+        }//Fin Para
+    }
+}
