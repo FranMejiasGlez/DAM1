392bef08aab15e363225de0a47d9e23a0632af16
FranDAM1 <franciscomejias.24@campuscamara.es>
2025-05-19 14:54:07 +0200

    .

---
+++ PROG/Proyectos NetBeans/Stream/pruebas/src/LeerByte.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Mejias Gonzalez Francisco
+ */
+public class LeerByte {
+    //Atributos
+    private byte teclado;
+    //Constructores
+    public LeerByte(){
+    }
+    //Metodos
+}//Fin Clase
